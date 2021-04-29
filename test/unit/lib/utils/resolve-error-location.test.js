812912902f6939483daeddc50c8c6ed93b0e6f8e
Piotr Grzesik <pj.grzesik@gmail.com>
2021-04-29 15:12:38 +0200

    refactor: Introduce `resolveErrorLocation` util

---
+++ test/unit/lib/utils/resolve-error-location.test.js
@@ -0,0 +1,56 @@
+'use strict';
+
+const expect = require('chai').expect;
+
+const ServerlessError = require('../../../../lib/serverless-error');
+const resolveErrorLocation = require('../../../../lib/utils/resolve-error-location');
+
+describe('test/unit/lib/utils/resolve-error-location.test.js', () => {
+  it('should be null for user error with code', () => {
+    const err = new ServerlessError('test', 'ERR_CODE');
+    const result = resolveErrorLocation(err);
+    expect(result).to.be.null;
+  });
+
+  it('should be null when stack missing', () => {
+    const err = new Error('test');
+    delete err.stack;
+    const result = resolveErrorLocation(err);
+    expect(result).to.be.null;
+  });
+
+  it('should be null for error with code and one-line stacktrace', () => {
+    const err = new Error('test');
+    err.code = 'ERR_CODE';
+    err.stack = 'Oneline stacktrace';
+    const result = resolveErrorLocation(err);
+    expect(result).to.be.null;
+  });
+
+  it('should be null if no matching lines found', () => {
+    const err = new Error('test');
+    err.stack = 'no matching\nlines in\nstacktrace';
+    const result = resolveErrorLocation(err);
+    expect(result).to.be.null;
+  });
+
+  it('should return at most 5 lines', () => {
+    const err = new Error('test');
+    err.stack =
+      'Error:\n' +
+      '    at Context.it (/home/xxx/serverless/test/unit/lib/utils/resolve-error-location.test.js:10:17)\n' +
+      '    at callFn (/home/xxx/serverless/node_modules/mocha/lib/runnable.js:366:21)\n' +
+      '    at Test.Runnable.run (/home/xxx/serverless/node_modules/mocha/lib/runnable.js:354:5)\n' +
+      '    at Runner.runTest (/home/xxx/serverless/node_modules/mocha/lib/runner.js:677:10)\n' +
+      '    at next (/home/xxx/serverless/node_modules/mocha/lib/runner.js:801:12)\n' +
+      '    at next (/home/xxx/serverless/node_modules/mocha/lib/runner.js:594:14)\n';
+    const result = resolveErrorLocation(err);
+    expect(result).to.deep.equal([
+      '/test/unit/lib/utils/resolve-error-location.test.js:10:17',
+      '/node_modules/mocha/lib/runnable.js:366:21',
+      '/node_modules/mocha/lib/runnable.js:354:5',
+      '/node_modules/mocha/lib/runner.js:677:10',
+      '/node_modules/mocha/lib/runner.js:801:12',
+    ]);
+  });
+});
